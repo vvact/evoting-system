45007bf1b1c0746c8e830cbff0bb53a9f203f85c
Victor Bunyali <vbm7752@gmail.com>
2026-03-19 06:00:17 +0300

    new commit

--- votes/admin.py
+++ votes/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Vote
 
+# 🔥 Admin Branding
+admin.site.site_header = "E-Voting System Admin"
+admin.site.site_title = "E-Voting Portal"
+admin.site.index_title = "Welcome to the E-Voting Dashboard"
+
 
 @admin.register(Vote)
 class VoteAdmin(admin.ModelAdmin):
